fa0f63999d61dd2ace91d2b39f14fbee71c7f53c
skakun,a <skakun2025@mail.ru>
2025-02-15 12:12:48 +0300

    changes

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -14,6 +14,7 @@ var (
 func parseFlags() {
 	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&flagBaseURL, "b", "http://localhost:8080/", "base URL for shortened links")
+
 	flag.Parse()
 
 	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
