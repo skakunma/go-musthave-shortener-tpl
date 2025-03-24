5cf04685d62f15dbab1a7b6b5b4b3755d7fbbcda
skakun,a <skakun2025@mail.ru>
2025-03-24 18:12:55 +0300

    commmit

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,12 @@
 package config
 
 import (
+	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"sync"
 
 	"github.com/skakunma/go-musthave-shortener-tpl/internal/storage"
@@ -14,6 +17,12 @@ import (
 var Cfg *Config
 
 type (
+	ShortenTextFile struct {
+		UUID        string `json:"uuid"`
+		ShortURL    string `json:"short_url"`
+		OriginalURL string `json:"original_url"`
+		UserID      int    `json:"user_id"`
+	}
 	Config struct {
 		Mu             sync.Mutex
 		Charset        string
@@ -28,6 +37,34 @@ type (
 	}
 )
 
+func LoadLinksFromFile(ctx context.Context, cfg *Config) error {
+
+	file, err := os.Open(cfg.FlagPathToSave)
+	if err != nil {
+		return fmt.Errorf("не удалось открыть файл: %w", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var link ShortenTextFile
+		err := json.Unmarshal(scanner.Bytes(), &link)
+		if err != nil {
+			return fmt.Errorf("ошибка парсинга JSON: %w", err)
+		}
+		uuid := strconv.Itoa(cfg.Store.Len(ctx) - 1)
+		userID := link.UserID
+
+		cfg.Store.Save(ctx, uuid, link.ShortURL, link.OriginalURL, userID)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("ошибка чтения файла: %w", err)
+	}
+
+	return nil
+}
+
 func LoadConfig(ctx context.Context) (*Config, error) {
 	cfg := &Config{
 		Charset:       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -40,7 +77,8 @@ func LoadConfig(ctx context.Context) (*Config, error) {
 		return nil, fmt.Errorf("ошибка инициализации логгера: %w", err)
 	}
 	cfg.Sugar = logger.Sugar()
-	ParseFlags(cfg)
+
+	// Выбираем хранилище (PostgreSQL или in-memory)
 	if cfg.FlagForDB != "" {
 		pgStorage, err := storage.NewPostgresStorage(cfg.FlagForDB)
 		if err != nil {
@@ -52,12 +90,13 @@ func LoadConfig(ctx context.Context) (*Config, error) {
 		cfg.Store = storage.NewLinkStorage()
 	}
 
+	// Открываем файл для записи
 	cfg.File, err = os.OpenFile(cfg.FlagPathToSave, os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		cfg.Sugar.Errorf("Ошибка открытия файла: %v", err)
 	}
 
-	if err := storage.LoadLinksFromFile(ctx, cfg.Store, cfg.FlagPathToSave); err != nil {
+	if err := LoadLinksFromFile(ctx, cfg); err != nil {
 		cfg.Sugar.Error("Ошибка загрузки ссылок:", err)
 	}
 
